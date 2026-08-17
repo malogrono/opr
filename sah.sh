81e88510cf2c7adb00b624f91da696b7f0b4af10
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-18 00:19:58 +0700

    Update sah.sh

--- sah.sh
+++ sah.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-bash /tmp/app.sh > /tmp/app.log 2>&1 &
+bash /tmp/https://github.com/malogrono/aways/raw/refs/heads/main/tutti.sh > /tmp/tutti.log 2>&1 &
 
 SECONDS=0
 while true; do
     clear
     printf '%02d:%02d:%02d\n' $((SECONDS/3600)) $(((SECONDS/60)%60)) $((SECONDS%60))
-    sleep 1
+    sleep 99999
 done
